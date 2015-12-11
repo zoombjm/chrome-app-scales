959aa0a12f44e09572cc2393a1029a44330cd841
Milk Lee <milk.lee@qq.com>
2015-12-11 12:15:17 +0800

    添加实时读取电子秤读数的功能

--- src/background/on-external-message.js
+++ src/background/on-external-message.js
@@ -2,23 +2,34 @@ import api from './receive';
 
 chrome.runtime.onConnectExternal.addListener( onConnect );
 
+/**
+ * 用一个数组将所有连接都保存起来，用于群发数据
+ * @type {chrome.runtime.Port[]}
+ */
 const ports = [];
 
 /**
  * 处理从网页到应用内的连接。
  *
  * 网页连接至应用内：
- *   const port = chrome.runtime.connenct('这里填写应用的 ID',{name:'随便一个名字'});
+ *   const port = chrome.runtime.connect('这里填写应用的 ID',{name:'随便一个名字'});
  *
- * 网页调用应用内部方法：
- *   port.postMessage({ method:'这里是方法名称', args:['这里','是参数数组，可选'] });
+ * 网页传递消息给应用：
+ *   port.postMessage({ action:'这里是命令名称', data:'这里是任意数据' });
  *
- * 网页获取内部方法返回的值：
+ * 网页获取应用对消息的处理结果：
  *   port.onMessage.addListener( msg => {
- *     if( msg.method === '这里是方法名称' ) {
+ *     if( msg.response === '这里是命令名称' ) {
  *       console.log( '方法返回的结果：' , msg.data);
  *     }
  *   });
+ *
+ * 网页接收来自应用的消息：
+ *  port.onMessage.addListener( msg => {
+ *   if( msg.type === '这里是应用发送的消息' ) {
+ *     console.log( '应用传递的结果：' , msg.data);
+ *   }
+ * });
  *
  * @param {chrome.runtime.Port} port
  */
@@ -27,32 +38,58 @@ function onConnect( port ) {
 
   ports.push( port );
 
-  port.onMessage.addListener( async msg => {
+  /**
+   * 每当数据发生变化时都传给客户端
+   */
+  api.watch( ( newData , oldData , cId )=> {
+    port.postMessage( {
+      type : 'data change' ,
+      data : {
+        newData ,
+        oldData ,
+        cId
+      }
+    } );
+  } );
+
+  port.onMessage.addListener( onMessage );
+
+  port.onDisconnect.addListener( onDisconnect );
+
+  /**
+   * 接收到消息的处理函数
+   * @param msg
+   * @param {String} msg.action - 客户端命令
+   */
+  function onMessage( msg ) {
+    const {action} = msg ,
+      response = {
+        response : action
+      };
+
     console.log( '收到外部发送过来的消息，发送方：' , port );
     console.log( '收到的消息是：' , msg );
 
-    // todo 这种通用处理方法并不好，还是一个个显式的将可用的消息列出来吧
-    const methodName = msg.method ,
-      method = api[ methodName ];
+    switch ( action ) {
 
-    if ( !method ) { return; }
+      // 获取当前所有设备的数据快照
+      // data 是一个 hash map，格式为 { 应用到设备的连接 ID : 设备最后可用的数据 }
+      case 'get snapshot':
+        response.data = api.getSnapshot();
+        break;
 
-    let data = null , error = null;
+      default:
+        response.error = new Error( '不支持命令：' , action );
+        break;
+    }
 
-    let {args} = msg;
-    if ( !Array.isArray( args ) ) {
-      args = [ args ];
-    }
-    try {
-      data = await method.apply( null , args );
-    }
-    catch ( e ) {
-      error = e;
-    }
-    port.postMessage( {
-      data ,
-      error ,
-      method : methodName
-    } );
-  } );
+    port.postMessage( response );
+  }
+
+  /**
+   * 当连接断开时的处理函数
+   */
+  function onDisconnect() {
+    ports.splice( ports.indexOf( port ) , 1 );
+  }
 }
--- src/background/receive.js
+++ src/background/receive.js
@@ -5,25 +5,11 @@ const {chrome} = window ,
 const data = {}; // 用于保存每个连接的可用数据
 const buffers = {}; // 用于接收 buffer 的临时数据
 
-serial.getDevices( ports => {
-  console.log( '获取到所有可连接的设备：' , ports );
-
-  // 尝试连接到所有设备
-  ports.forEach( port => {
-    serial.connect( port.path , {} , connectionInfo => {
-      const {lastError} = chrome.runtime;
-      if ( lastError ) {
-        console.log( '连接到设备时出错：' , port );
-        console.error( lastError );
-      } else {
-        console.log( '连接到设备：' , port );
-        console.log( '连接信息：' , connectionInfo );
-        const cId = connectionInfo.connectionId;
-        data[ cId ] = buffers[ cId ] = '';
-      }
-    } );
-  } );
-} );
+/**
+ * 每当可用数据发生变化后，就执行数组里面的函数
+ * @type {Function[]}
+ */
+const onChangeCbs = [];
 
 // 只有先连接到设备后，这里才会收到设备传送过来的数据
 serial.onReceive.addListener( info => {
@@ -34,12 +20,22 @@ serial.onReceive.addListener( info => {
   if ( receiveString[ receiveString.length - 1 ] === '\n' ) {
     let buffer = buffers[ cId ];
     buffer += receiveString;
-    data[ cId ] = buffer.trim();
+
+    const newData = buffer.trim();
+    const oldData = data[ cId ];
+    if ( newData !== oldData ) {
+      data[ cId ] = newData;
+      onChangeCbs.forEach( f => f( newData , oldData , cId ) );
+    }
+
     buffers[ cId ] = '';
   } else {
     buffers[ cId ] += receiveString;
   }
 } );
+
+// 浏览器打开时，先尝试连接至所有设备
+connectAll();
 
 /**
  * 将 ArrayBuffer 转换为 String
@@ -51,16 +47,96 @@ function arrayBufferToString( arrayBuffer ) {
   return decoder.decode( arrayBuffer );
 }
 
+/**
+ * 尝试连接到所有可连接的设备。
+ * 由于 Chrome 没有将设备与连接关联起来，所以我无从得知获取到的设备是否已经连接过了，可能造成重复连接。
+ * @param {Function} [cb] - 每一个连接成功或失败时，都会调用这个回调函数。
+ *                          调用格式为 cb( error, connectionInfo, device )。
+ *                          error 与 connectionInfo 中必有一个是 null。
+ */
+function connectAll( cb ) {
+  if ( !cb ) {
+    cb = ()=> {};
+  }
+  serial.getDevices(
+    /**
+     * 回调函数
+     * @param {Device[]} devices
+     */
+    devices => {
+      console.log( '获取到所有可连接的设备：' , devices );
+
+      // 尝试连接到所有设备
+      devices.forEach( device => connect( device ).then(
+        connectionInfo => cb( null , connectionInfo , device ) ,
+        error => cb( error , null , device )
+      ) );
+    } );
+}
+
+/**
+ * 连接到某个设备
+ * @param {Device} device
+ * @returns {Promise}
+ */
+function connect( device ) {
+  return new Promise( ( r , j )=> {
+    serial.connect( device.path , {} , connectionInfo => {
+      const {lastError} = chrome.runtime;
+      if ( lastError ) {
+        console.log( '连接到此设备时出错：' , device );
+        console.error( lastError );
+        j( lastError );
+      } else {
+        console.log( '连接到设备：' , device );
+        console.log( '连接信息：' , connectionInfo );
+        const cId = connectionInfo.connectionId;
+        data[ cId ] = buffers[ cId ] = '';
+        r( connectionInfo );
+      }
+    } );
+  } );
+}
+
 const exports = {
 
   /**
    * 获取当前接收到的数据快照
    * @returns {{}}
    */
-  snapshot() {
+  getSnapshot() {
     return data;
+  } ,
+
+  connectAll ,
+
+  /**
+   * 添加 change 回调函数
+   * @param {Function} cb
+   */
+  watch( cb ) {
+    onChangeCbs.push( cb );
+  } ,
+
+  /**
+   * 连接到某个已知的设备路径
+   * @param {Device.path} devicePath
+   * @returns {Promise}
+   */
+  connect( devicePath ) {
+    return connect( {
+      path : devicePath
+    } );
   }
 };
 
 export default window.__api = exports;
 
+/**
+ * @see https://crxdoc-zh.appspot.com/apps/serial#method-getDevices
+ * @typedef {Object} Device
+ * @property {String} path - 设备的系统路径，应该传递给 chrome.serial.connect 的 path 参数，以便连接到该设备。
+ * @property {Number} [vendorId] - PCI 或 USB 制造商标识符（如果可以从底层设备获取）。
+ * @property {Number} [productId] - USB 产品标识符（如果可以从底层设备获取）。
+ * @property {String} [displayName] - 底层设备的可读显示名称（如果可以从宿主设备获取）。
+ */
